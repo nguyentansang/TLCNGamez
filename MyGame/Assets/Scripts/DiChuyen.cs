62ea158f6855e1202c5c16ebe503ed0c978512e1
Blue <32140181+BlueVT1437@users.noreply.github.com>
2019-11-13 12:03:25 +0700

    tientrinh2

---
+++ MyGame/Assets/Scripts/DiChuyen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiChuyen : MonoBehaviour
+{
+    public float moveSpeed=1;
+    public float moveRange=3;
+
+    private Vector3 oldPositon;
+    private GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject;
+        oldPositon = obj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(-1 * Time.deltaTime * moveSpeed, 0, 0));
+
+        if (Vector3.Distance(oldPositon, obj.transform.position) > moveRange) {
+            obj.transform.position = oldPositon;
+        }
+    }
+}
