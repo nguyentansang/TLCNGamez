12e85414578851dcd4f29655b514c1fa96f5f731
Blue <32140181+BlueVT1437@users.noreply.github.com>
2019-11-18 23:29:14 +0700

    update

--- MyGame/Assets/Scripts/NhanVat.cs
+++ MyGame/Assets/Scripts/NhanVat.cs
@@ -4,86 +4,30 @@ using UnityEngine;
 
 public class NhanVat : MonoBehaviour
 {
-    public float jumpPower;
-    public float movePower = 20f;
-    public float maxVelocity = 4f;
+    public float speed = 30f, maxspeed = 3, jumpPow = 250f;
+    //public float maxVelocity = 4f;
 
     private bool grounded;
-    private Rigidbody2D myBody;
+    private Rigidbody2D r2;
     private Animator anim;
     GameObject obj;
 
-    void Awake() {
-        myBody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-    }
+    
     // Start is called before the first frame update
     void Start()
     {
         obj = gameObject;
-        jumpPower = 300f;
+        jumpPow = 300f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetMouseButton(0)) {
-        //    obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpPower));
-        //}
-        PlayerWalkKeyBoard();
-    }
-
-    void PlayerWalkKeyBoard() {
-        float forceX = 0f;
-        float forceY = 0f;
-
-        float vel = Mathf.Abs(myBody.velocity.x);
-        float h = Input.GetAxisRaw("Horizontal"); //A <- : -1 , D -> : 1
-
-        if (h > 0) {
-            if(vel < maxVelocity)
-            {
-                forceX = movePower;
-            }
-            Vector3 scale = transform.localScale;
-            scale.x=1f;
-            transform.localScale = scale;
-
-            anim.SetBool("Walk", true);
-        }
-        else if (h > 0) {
-            if (vel < maxVelocity)
-            {
-                forceX = -movePower;
-            }
-            Vector3 scale = transform.localScale;
-            scale.x = -1f;
-            transform.localScale = scale;
-
-            anim.SetBool("Walk", true);
-        }
-        else if (h == 0)
+        if (Input.GetMouseButton(0))
         {
-            anim.SetBool("Walk", false);
+            obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpPow));
         }
-
-        if(Input.GetKey(KeyCode.Space))
-        {
-            if (grounded)
-            {
-                grounded = false;
-                forceY = jumpPower;
-            }
-        }
-
-        myBody.AddForce(new Vector2(forceX, forceY));
+        
     }
-
-    void OnColliSionEnter2d(Collision2D target)
-    {
-        if(target.gameObject.tag == "ground")
-        {
-            grounded = true;
-        }
-    }
+    
 }
---
+++ MyGame/Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 30f, maxspeed = 3, jumpPow = 250f;
+    public bool grounded = true, faceright= true, doublejump=false;
+
+    public Rigidbody2D r2;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2 = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("grounded", grounded);
+        anim.SetFloat("Speed", Mathf.Abs(r2.velocity.x));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                grounded = false;
+                r2.AddForce(Vector2.up * jumpPow);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        r2.AddForce((Vector2.right) * speed * h);
+
+        if(r2.velocity.x > maxspeed)
+        {
+            r2.velocity = new Vector2(maxspeed, r2.velocity.y);
+        }
+        if (r2.velocity.x < -maxspeed)
+        {
+            r2.velocity = new Vector2(-maxspeed, r2.velocity.y);
+        }
+
+        if(h>0 && !faceright)
+        {
+            Flip();
+        }
+        else if (h < 0 && faceright)
+        {
+            Flip();
+        }
+    }
+    public void Flip()
+    {
+        faceright = !faceright;
+        Vector3 Scale;
+        Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+    }
+}
